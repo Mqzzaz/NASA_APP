55c879ce6b58dd1d4fc8cc62bebd74b67f3ba54c
Mushabab Alasmari <masmari@mushababs-macbook-air.local>
2018-02-22 16:23:36 +0300

    Finish communicating with API but still working on integration with the screen.

--- NASA_APP/ViewController.swift
+++ NASA_APP/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nasaImage: UIImageView!
+    @IBOutlet weak var credit: UILabel!
+    @IBOutlet weak var textTitle: UILabel!
+    @IBOutlet weak var text: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getNASA_Data()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //TODO: didnt work....why????
+    func getNASA_Data()
+    {
+        let API_key = "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
+        let url = "https://api.nasa.gov/planetary/apod?api_key=\(API_key)"
+        Alamofire.request(url).response { response in
+            
+            let json = JSON(response.data!)
+            
+            let url = URL(string : json["url"].stringValue)
+            let imageData = try? Data(contentsOf: url!)
+            self.nasaImage.image = UIImage(data: imageData!)
+            self.credit.text = json["copyright"].stringValue
+            self.textTitle.text = json["title"].stringValue
+            self.text.text = json["explanation"].stringValue
+            
+        }
     }
-
-
 }
 
